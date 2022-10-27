01dc95c7ea6ae7c07844ec3f6e95fa9d746d8791
balebom <binhnguyen.balebom.dev@gmail.com>
2022-10-27 08:53:36 +0700

    update repos

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class ProductImage extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable, HasApiTokens;
+
+    protected $fillable = [
+        'image_url',
+        'product_id',
+    ];
+
+    protected $hidden = [];
+
+    protected $casts = [];
 }
